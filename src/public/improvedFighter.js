d33faca0884b1cc343aac2c82e589618c233440d
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-21 19:52:02 +0300

    Created improvedFighter

--- src/public/improvedFighter.js
+++ src/public/improvedFighter.js
@@ -1,1 +1,15 @@
 // Create class ImprovedFighter
+import { Fighter } from "./fighter.js";
+
+// Create class ImprovedFighter
+export class ImprovedFighter extends Fighter {
+  constructor(_name, _health, _power) {
+    super(_name, _health, _power);
+  }
+
+  doubleHit(enemy, point) {
+    let doubleDamage = 0;
+    doubleDamage = 2 * point;
+    this.hit(enemy, doubleDamage);
+  }
+}
