70ccbfac7ebd669632aff2061d6ba77c993b383c
PavloPaitak <38991419+PavloPaitak@users.noreply.github.com>
2018-07-21 20:04:39 +0300

    Created index.js

--- src/public/index.js
+++ src/public/index.js
@@ -1,3 +1,9 @@
-// create two instances
+import { Fighter } from "./fighter";
+import { ImprovedFighter } from "./improvedFighter";
+import { fight } from "./fight";
 
+// create two instances
+var fighter = new Fighter("Fighter", 100, 3);
+var improvedFighter = new ImprovedFighter("improvedFighter", 100, 3);
 // call fight function
+fight(fighter, improvedFighter, 25, 13, 45);
